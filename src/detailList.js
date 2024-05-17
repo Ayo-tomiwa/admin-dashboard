db317d12b3ff45682fe71776044029f05cc566eb
Tomiwa <oyindml1@gmail.com>
2024-05-17 14:32:52 +0100

    commit

--- src/detailList.js
+++ src/detailList.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import './detailList.css'
+import { RiCheckboxBlankLine } from "react-icons/ri"
 
 function DetailList() {
   return (
-    <div>DetailList</div>
+    <div className='detail-list-wrapper'>
+        <div className='detail-list-row'>
+            <div className='cont'>
+                <div className='header-text-icon'>
+                <RiCheckboxBlankLine />
+                </div>
+                <div className='header-text'>
+                    <p>Account ID</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 

24b7cf722a520ae2ebc5bef4505bf86a6a31de65
Tomiwa <oyindml1@gmail.com>
2024-06-14 17:36:07 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import React from 'react'
 import Sidebar from './sidebar'
 import Main from './Main'
 import './App.css'
+import {Helmet} from "react-helmet";
 
 function App() {
   return (
+   
+
     <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Byte App</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <meta name="description" content="Byte Web App" />
+    </Helmet>
     <div className='main'>
     <Sidebar />
       <Main />
